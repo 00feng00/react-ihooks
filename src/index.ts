be865d463a43da17a6773c1f15cff4946a6717cf
feng <892751427@qq.com>
2022-02-17 23:10:42 +0800

    feat: 导出函数;

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
-export { default as Foo } from './hooks/src/Foo';
+import Foo from './hooks/src/Foo';
+import useDocumentTitle from './hooks/src/UseDocumentTitle';
+
+export { Foo, useDocumentTitle };
