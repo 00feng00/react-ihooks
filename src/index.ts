b55ed9434e8cdb343838ffa4437a7502994019c9
feng <892751427@qq.com>
2022-02-28 23:40:08 +0800

    feat: 更换背景图;

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import useDocumentTitle from './hooks/src/useDocumentTitle';
 import useDebounce from './hooks/src/useDebounce';
 import useThrottle from './hooks/src/useThrottle';
-export { useDebounce, useDocumentTitle, useThrottle };
+import useFullScreen from './hooks/src/useFullScreen';
+export { useDebounce, useDocumentTitle, useThrottle, useFullScreen };
